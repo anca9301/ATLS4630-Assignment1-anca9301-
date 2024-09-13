b7adc8ff6917d08cc96dc31d4059ceaa60deb13d
Annie <anca9301@colorado.edu>
2024-09-13 12:53:12 -0600

    complete general html and js

---
+++ js/script.js
@@ -0,0 +1,101 @@
+const BODY = document.querySelector('body');
+const YOUR_FORM = document.querySelector('yourForm');
+const DATES_FORM = document.querySelector('datesForm');
+
+const BACKGROUND_COLOR_INPUT = document.getElementById('backgroundColorInput');
+const FONT_STYLE_INPUT = document.getElementById('fontStyleInput');
+
+const NAME_INPUT = document.getElementById('nameInput');
+const DOB_INPUT = document.getElementById('birthDateInput');
+const END_DATE_INPUT = document.getElementById('endDateInput');
+
+const FROM_DATE_INPUT = document.getElementById('fromDateInput');
+const TO_DATE_INPUT = document.getElementById('toDateInput');
+
+const CUR_AGE_BUTTON = document.getElementById('calcCurAgeButton');
+const AGE_AT_BUTTON = document.getElementById('calcAgeAt');
+const TIME_DIFF_BUTTON = document.getElementById('calcTimeDiffButton');
+
+BACKGROUND_COLOR_INPUT.addEventListener('input', setBackgroundColor);
+FONT_STYLE_INPUT.addEventListener('input', setfontStyle);
+
+CUR_AGE_BUTTON.addEventListener('click', calcCurrentAge);
+AGE_AT_BUTTON.addEventListener('click', calcAgeAt);
+TIME_DIFF_BUTTON.addEventListener('click', calcDifference);
+
+function setBackgroundColor() {
+    // 8B8680 - mid gray
+    // console.log(backgroundColorInput.value);
+    // let midGray = Number('0x8B8680');
+    const CHANGE_COLOR = BACKGROUND_COLOR_INPUT.value.substring(1, 7);
+    let midHexDeci = 8388607;
+
+    BODY.style.backgroundColor = BACKGROUND_COLOR_INPUT.value;
+
+    if (Number(`0x${CHANGE_COLOR}`) > midHexDeci) {
+        BODY.style.color = "black";
+    }
+    else {
+        BODY.style.color = "white";
+    }
+}
+
+function setfontStyle() {
+    const CHANGE_STYLE = FONT_STYLE_INPUT.value;
+
+    BODY.style.fontFamily = CHANGE_STYLE;
+}
+
+function calcTimeDifference(startDate, endDate) {
+    const START = Date.parse(startDate);
+    const END = Date.parse(endDate);
+
+    const MILLSEC_PER_SEC_PER_DAY = 86_400_000;
+    const DAYS_PER_YEAR = 365.25;
+    const DAYS_PER_MONTH = 30.5;
+
+    const DIFF_IN_DAYS = (END - START) / MILLSEC_PER_SEC_PER_DAY;
+    const CALC_YEARS = Math.floor(DIFF_IN_DAYS / DAYS_PER_YEAR);
+    const CALC_MONTHS = Math.floor((DIFF_IN_DAYS - CALC_YEARS * DAYS_PER_YEAR) / DAYS_PER_MONTH);
+    const CALC_DAYS = Math.floor(DIFF_IN_DAYS - CALC_YEARS * DAYS_PER_YEAR - CALC_MONTHS * DAYS_PER_MONTH);
+
+    return `about ${CALC_YEARS} years, ${CALC_MONTHS} months, and ${CALC_DAYS} days`;
+}
+
+function calcCurrentAge(e) {
+    e.preventDefault();
+
+    const NAME = NAME_INPUT.value;
+    const DOB = DOB_INPUT.value;
+    const TODAY = Date();
+
+    const TIME_DIFF = calcTimeDifference(DOB, TODAY);
+    const AGE = `${NAME} was born ${TIME_DIFF} ago.`; 
+
+    console.log(AGE);
+}
+
+function calcAgeAt(e) {
+    e.preventDefault();
+
+    const NAME = NAME_INPUT.value;
+    const DOB = DOB_INPUT.value;
+    const END = END_DATE_INPUT.value;
+
+    const TIME_DIFF = calcTimeDifference(DOB, END);
+    const AGE = `On ${END}, ${NAME} will be ${TIME_DIFF} old.`; 
+
+    console.log(AGE);
+}
+
+function calcDifference(e) {
+    e.preventDefault();
+
+    const FROM = FROM_DATE_INPUT.value;
+    const TO = TO_DATE_INPUT.value;
+
+    const TIME_DIFF = calcTimeDifference(FROM, TO);
+    const DIFF = `The difference from ${FROM} to ${TO} is ${TIME_DIFF}.`; 
+
+    console.log(DIFF);
+}
